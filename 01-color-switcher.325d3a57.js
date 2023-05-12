05df87787038d35eb3b2800c143bd6b2e023c51a
Vikki2Rikki <119513480+Vikki2Rikki@users.noreply.github.com>
2023-05-12 03:21:35 +0000

    Deploying to gh-pages from @ Vikki2Rikki/goit-js-hw-09@be72af02f834b1fccbb3b0e61f8e068b1e34b306 🚀

---
+++ 01-color-switcher.325d3a57.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;t.addEventListener("click",(function(e){n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),t.setAttribute("disabled","disabled")}),1e3)})),e.addEventListener("click",(function(e){clearInterval(n),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.325d3a57.js.map
